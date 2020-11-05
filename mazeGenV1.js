c54e2f27f8e74ce98ad1ce3efa1ea70a0db9c341
lulange <lucas@langes.us>
2020-11-05 14:01:19 -0500

    protect the class values with getters

--- mazeGenV1.js
+++ mazeGenV1.js
@@ -1,25 +1,25 @@
 class Maze {
   constructor(canvasID, width, height, randomness, start, end) {
-    this.canvas = document.getElementById(canvasID);
-    this.ctx = canvas.getContext("2d");
-    this.width = width || 10;
-    this.height = height || this.width;
-    this.randomness = randomness || 10;
-    this.start = start || {x: 0, y: 1};
-    this.end = end || {x: -1, y: -2};
-    this.maze = [];
+    this._canvas = document.getElementById(canvasID);
+    this._ctx = canvas.getContext("2d");
+    this._width = width || 10;
+    this._height = height || this._width;
+    this._randomness = randomness || 10;
+    this._start = start || {x: 0, y: 1};
+    this._end = end || {x: -1, y: -2};
+    this._maze = [];
     let stack = [];
-    for (let i=0; i<this.height*2 + 1; i++) {
-      this.maze.push([]);
-      for (let j=0; j<this.width*2 + 1; j++) {
-        this.maze[i].push(1);
+    for (let i=0; i<this._height*2 + 1; i++) {
+      this._maze.push([]);
+      for (let j=0; j<this._width*2 + 1; j++) {
+        this._maze[i].push(1);
       }
     }
-    let xAndYStart = Math.floor(this.width/2);
+    let xAndYStart = Math.floor(this._width/2);
     if (xAndYStart % 2 !== 1) {
       xAndYStart++;
     }
-    this.maze[xAndYStart][xAndYStart] = 0;
+    this._maze[xAndYStart][xAndYStart] = 0;
     stack = [{x: xAndYStart, y: xAndYStart}];
 
 
@@ -27,7 +27,7 @@ class Maze {
     while (stack.length > 0) {
       let randomSelector = Math.random();
       let selectedVertexIndex;
-      if (randomSelector > this.randomness/100) {
+      if (randomSelector > this._randomness/100) {
         selectedVertexIndex = stack.length - 1;
       } else {
         selectedVertexIndex = Math.floor(Math.random() * stack.length);
@@ -37,25 +37,25 @@ class Maze {
       let possibleExt = [];
       // left
       if (selectedVertex.x !== 1) {
-        if (this.maze[selectedVertex.y][selectedVertex.x - 2] === 1) {
+        if (this._maze[selectedVertex.y][selectedVertex.x - 2] === 1) {
           possibleExt.push({x: selectedVertex.x - 2, y: selectedVertex.y});
         }
       }
       // right
-      if (selectedVertex.x !== this.maze[0].length - 2) {
-        if (this.maze[selectedVertex.y][selectedVertex.x + 2] === 1) {
+      if (selectedVertex.x !== this._maze[0].length - 2) {
+        if (this._maze[selectedVertex.y][selectedVertex.x + 2] === 1) {
           possibleExt.push({x: selectedVertex.x + 2, y: selectedVertex.y});
         }
       }
       // down
       if (selectedVertex.y !== 1) {
-        if (this.maze[selectedVertex.y - 2][selectedVertex.x] === 1) {
+        if (this._maze[selectedVertex.y - 2][selectedVertex.x] === 1) {
           possibleExt.push({x: selectedVertex.x, y: selectedVertex.y - 2});
         }
       }
       // up
-      if (selectedVertex.y !== this.maze.length - 2) {
-        if (this.maze[selectedVertex.y + 2][selectedVertex.x] === 1) {
+      if (selectedVertex.y !== this._maze.length - 2) {
+        if (this._maze[selectedVertex.y + 2][selectedVertex.x] === 1) {
           possibleExt.push({x: selectedVertex.x, y: selectedVertex.y + 2});
         }
       }
@@ -65,31 +65,31 @@ class Maze {
         stack.splice(selectedVertexIndex, 1);
       } else {
         let randomNum = Math.floor(Math.random() * possibleExt.length);
-        this.maze[possibleExt[randomNum].y][possibleExt[randomNum].x] = 0;
+        this._maze[possibleExt[randomNum].y][possibleExt[randomNum].x] = 0;
         // below
         if (possibleExt[randomNum].y > selectedVertex.y) {
-          this.maze[possibleExt[randomNum].y-1][possibleExt[randomNum].x] = 0;
+          this._maze[possibleExt[randomNum].y-1][possibleExt[randomNum].x] = 0;
         // above
         } else if (possibleExt[randomNum].y < selectedVertex.y) {
-          this.maze[possibleExt[randomNum].y+1][possibleExt[randomNum].x] = 0;
+          this._maze[possibleExt[randomNum].y+1][possibleExt[randomNum].x] = 0;
         // left
         } else if (possibleExt[randomNum].x > selectedVertex.x) {
-          this.maze[possibleExt[randomNum].y][possibleExt[randomNum].x-1] = 0;
+          this._maze[possibleExt[randomNum].y][possibleExt[randomNum].x-1] = 0;
         // right
         } else if (possibleExt[randomNum].x < selectedVertex.x) {
-          this.maze[possibleExt[randomNum].y][possibleExt[randomNum].x+1] = 0;
+          this._maze[possibleExt[randomNum].y][possibleExt[randomNum].x+1] = 0;
         }
         stack.push(possibleExt[randomNum]);
       }
     }
 
-    if (this.start.x >= this.maze[0].length || this.start.y >= this.maze.length || this.maze.length + this.end.y < 0 || this.maze[0].length + this.end.x < 0) {
+    if (this._start.x >= this._maze[0].length || this._start.y >= this._maze.length || this._maze.length + this._end.y < 0 || this._maze[0].length + this._end.x < 0) {
       console.log("invalid start/end object: exceeds the maze variable length");
     } else {
-      if ((this.start.x+2) % 2 === 1 || (this.start.y+2) % 2 === 1) {
-        if (((this.end.x*-1)+2) % 2 === 1 || ((this.end.y*-1)+2) % 2 === 1) {
-          this.maze[this.start.y][this.start.x] = 0;
-          this.maze[this.maze.length+this.end.y][this.maze[0].length+this.end.x] = 0;
+      if ((this._start.x+2) % 2 === 1 || (this._start.y+2) % 2 === 1) {
+        if (((this._end.x*-1)+2) % 2 === 1 || ((this._end.y*-1)+2) % 2 === 1) {
+          this._maze[this._start.y][this._start.x] = 0;
+          this._maze[this._maze.length+this._end.y][this._maze[0].length+this._end.x] = 0;
         } else {
           console.log("invalid start/end object: must contain an odd number");
         }
@@ -106,29 +106,29 @@ class Maze {
     if (wallColor === undefined || wallColor === null) {
       wallColor = "black";
     }
-    this.ctx.fillStyle = wallColor;
-    let totalHeight = Math.round(this.maze.length/2) * wallWidth + Math.floor(this.maze.length/2) * boxWidth;
-    let totalWidth = Math.round(this.maze[0].length/2) * wallWidth + Math.floor(this.maze[0].length/2) * boxWidth;
-    this.ctx.fillRect(x, y, totalWidth, totalHeight);
-    this.ctx.fillStyle = passageColor;
-    for (let i=0; i<this.maze.length; i++) {
-      for (let j=0; j<this.maze[0].length; j++) {
-        if (this.maze[i][j] === 0) {
+    this._ctx.fillStyle = wallColor;
+    let totalHeight = Math.round(this._maze.length/2) * wallWidth + Math.floor(this._maze.length/2) * boxWidth;
+    let totalWidth = Math.round(this._maze[0].length/2) * wallWidth + Math.floor(this._maze[0].length/2) * boxWidth;
+    this._ctx.fillRect(x, y, totalWidth, totalHeight);
+    this._ctx.fillStyle = passageColor;
+    for (let i=0; i<this._maze.length; i++) {
+      for (let j=0; j<this._maze[0].length; j++) {
+        if (this._maze[i][j] === 0) {
           let horizontalWall = i % 2 === 0 ? true : false;
           let verticalWall = j % 2 === 0 ? true : false;
 
           if (!verticalWall && !horizontalWall) {
             let pixelX = x + (j+1)/2 * wallWidth + (j-1)/2 * boxWidth;
             let pixelY = y + (i+1)/2 * wallWidth + (i-1)/2 * boxWidth;
-            this.ctx.fillRect(pixelX, pixelY, boxWidth, boxWidth);
+            this._ctx.fillRect(pixelX, pixelY, boxWidth, boxWidth);
           } else if (verticalWall) {
             let pixelX = x + (j/2) * wallWidth + (j/2) * boxWidth;
             let pixelY = y + (i+1)/2 * wallWidth + (i-1)/2 * boxWidth;
-            this.ctx.fillRect(pixelX, pixelY, wallWidth, boxWidth);
+            this._ctx.fillRect(pixelX, pixelY, wallWidth, boxWidth);
           } else {
             let pixelX = x + (j+1)/2 * wallWidth + (j-1)/2 * boxWidth;
             let pixelY = y + (i/2) * wallWidth + (i/2) * boxWidth;
-            this.ctx.fillRect(pixelX, pixelY, boxWidth, wallWidth);
+            this._ctx.fillRect(pixelX, pixelY, boxWidth, wallWidth);
           }
         }
       }
@@ -136,14 +136,46 @@ class Maze {
   }
 
   getWidth(boxWidth, wallWidth) {
-    return Math.round(this.maze[0].length/2) * wallWidth + Math.floor(this.maze[0].length/2) * boxWidth;
+    return Math.round(this._maze[0].length/2) * wallWidth + Math.floor(this._maze[0].length/2) * boxWidth;
   }
 
   getHeight(boxWidth, wallWidth) {
-    return Math.round(this.maze.length/2) * wallWidth + Math.floor(this.maze.length/2) * boxWidth;
+    return Math.round(this._maze.length/2) * wallWidth + Math.floor(this._maze.length/2) * boxWidth;
   }
 
   getPixelCoor(startX, startY, boxWidth, wallWidth, x, y) {
     return {x: startX + (x+1)/2 * wallWidth + (x-1)/2 * boxWidth, y: startY + (y+1)/2 * wallWidth + (y-1)/2 * boxWidth};
+  }
+
+  get ctx() {
+    return this._ctx;
+  }
+
+  get randomness() {
+    return this._randomness;
+  }
+
+  get end() {
+    return this._end;
+  }
+
+  get start() {
+    return this._start;
+  }
+
+  get maze() {
+    return this._maze;
+  }
+
+  get canvas() {
+    return this._canvas;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  get width() {
+    return this._width;
   }
 }
